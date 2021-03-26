1a83ec37424bc782d36b0d9de1ed1ed8a92058b2
Cody Kimb <codykimbell@gmail.com>
2021-03-25 19:13:31 -0500

    login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,29 @@ router.post('/', (req, res) => {
       });
   });
 
+// LOGIN ROUTE
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+
 // UPDATE USER /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
@@ -72,7 +95,7 @@ router.put('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
 
 // DELETE USER /api/users/1
 router.delete('/:id', (req, res) => {
@@ -92,6 +115,6 @@ router.delete('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
 
 module.exports = router;
