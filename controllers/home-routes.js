dc4c4e1e7e2594705b9dcc578d0bdb2a9ab7d026
Cody Kimb <codykimbell@gmail.com>
2021-03-25 23:40:30 -0500

    logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,6 +39,11 @@ router.get('/', (req, res) => {
 
 // LOGIN ROUTES
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
     res.render('login');
   });
 
--- server.js
+++ server.js
@@ -6,6 +6,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express-session and sequelize store
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
+
 //HANDLEBARS
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
