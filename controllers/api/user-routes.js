9486c06f7979b39a55690a88688394968687906c
Cody Kimb <codykimbell@gmail.com>
2021-03-26 22:20:37 -0500

    edit and delete post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,26 +55,31 @@ router.get('/:id', (req, res) => {
   });
 
 // POST NEW USER /api/users
-router.post('/', withAuth, (req, res) => {
-    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-    User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    })
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
     .then(dbUserData => {
       req.session.save(() => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-    
+  
         res.json(dbUserData);
       });
     })
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // LOGIN ROUTE
-router.post('/login', withAuth, (req, res) => {
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -93,18 +98,17 @@ router.post('/login', withAuth, (req, res) => {
     }
 
     req.session.save(() => {
-      // declare session variables
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
-
+  
       res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
   });
 });
 
 // LOGOUT ROUTE
-router.post('/logout', withAuth, (req, res) => {
+router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
@@ -114,6 +118,7 @@ router.post('/logout', withAuth, (req, res) => {
     res.status(404).end();
   }
 });
+
 
 // UPDATE USER /api/users/1
 router.put('/:id', withAuth, (req, res) => {
